1ed5c87518b051e66fa33b46c6a53954d4a3af1b
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-11-02 13:27:41 +0000

    Add files via upload

---
+++ week9/works.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    
+    <title>Welcome Screen</title>
+</head>
+<body>
+  <h1>Welcome to home screen</h1>
+<?php
+
+
+
+	require "my_add_functions.php";
+    
+  include "other_functions.php";
+
+echo "Value for add:  ". Add(5,2). ",     ";
+
+echo "";
+
+
+echo "Value of multiple: ". multiply(10.8);
+
+	
+	?>  
+    
+</body>
+</html>
